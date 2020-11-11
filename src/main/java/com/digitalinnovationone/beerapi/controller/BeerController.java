c484deb0db2c9753c4f60ac44c4733ddb44a4ef1
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-11-11 19:24:31 -0400

    Controller layer, API finished

---
+++ src/main/java/com/digitalinnovationone/beerapi/controller/BeerController.java
@@ -0,0 +1,59 @@
+package com.digitalinnovationone.beerapi.controller;
+
+import com.digitalinnovationone.beerapi.dto.BeerDTO;
+import com.digitalinnovationone.beerapi.dto.QuantityDTO;
+import com.digitalinnovationone.beerapi.exceptions.BeerNameAlreadyRegisteredException;
+import com.digitalinnovationone.beerapi.exceptions.BeerNotFoundException;
+import com.digitalinnovationone.beerapi.exceptions.BeerStockExceededException;
+import com.digitalinnovationone.beerapi.service.BeerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+public class BeerController {
+
+    private final BeerService beerService;
+
+    @Autowired
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerNameAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    public BeerDTO findByName(@PathVariable String beerName) throws BeerNotFoundException {
+        return beerService.findByName(beerName);
+    }
+
+    @GetMapping
+    public List<BeerDTO> listAll() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public BeerDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerStockExceededException, BeerNotFoundException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+
+    @PatchMapping("/{id}/decrement")
+    public BeerDTO decrement(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws BeerStockExceededException, BeerNotFoundException {
+        return beerService.decrement(id, quantityDTO.getQuantity());
+    }
+}
