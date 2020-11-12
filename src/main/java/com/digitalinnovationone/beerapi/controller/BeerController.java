474711af2f6d3ac969b1be1b0e38dcc495fa3321
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-11-12 15:51:45 -0400

    Adding Swagger Docs

--- src/main/java/com/digitalinnovationone/beerapi/controller/BeerController.java
+++ src/main/java/com/digitalinnovationone/beerapi/controller/BeerController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/beers")
-public class BeerController {
+public class BeerController implements BeerControllerDocs {
 
     private final BeerService beerService;
 
